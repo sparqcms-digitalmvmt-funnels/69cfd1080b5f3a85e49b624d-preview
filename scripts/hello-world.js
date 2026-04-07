5f9c42ef7e541f576b3125e7318dbbbe6f6731c6
Don E <DRElliston@users.noreply.github.com>
2026-04-07 06:55:54 -0500

    deploy funnel preview

--- scripts/hello-world.js
+++ scripts/hello-world.js
@@ -602,7 +602,7 @@ async function createOrderViaWallet(confirmationToken, paymentMethodId) {
         ?.getAttribute("data-shipping-profile-id") || undefined;
 
   const orderData = {
-    pageId: "iDJ0JoXQ9zwd5ElWcC62pUEgCXM4CfTlmQ6MqBFtmEyGSArow_SuBEp-wj8IgvaZ",
+    pageId: "gNRvxN-rQ2wRw5qbBtFp1AYaxskBAyJrPwVSBgkWDbkfCcHvXPagJt7BZIad3Cj7",
     action: "process",
     campaign_id: CAMPAIGN_ID,
     connection_id: 1,
@@ -1389,7 +1389,7 @@ async function createOrderViaPaypal(isExpress = false) {
   const shippingProfileId = +document.querySelector(`[data-product-id="${selectedProduct.id}"]`)?.getAttribute('data-shipping-profile-id') || undefined;
   const sameAddress = isSameAddress();
   const orderData = {
-    pageId: "iDJ0JoXQ9zwd5ElWcC62pUEgCXM4CfTlmQ6MqBFtmEyGSArow_SuBEp-wj8IgvaZ",
+    pageId: "gNRvxN-rQ2wRw5qbBtFp1AYaxskBAyJrPwVSBgkWDbkfCcHvXPagJt7BZIad3Cj7",
     action: "process",
     campaign_id: CAMPAIGN_ID,
     connection_id: 1, // VRIO URL ending /connection
@@ -1687,7 +1687,7 @@ async function createOrderViaKlarna() {
   const sameAddress = isSameAddress();
 
   const orderData = {
-    pageId: "iDJ0JoXQ9zwd5ElWcC62pUEgCXM4CfTlmQ6MqBFtmEyGSArow_SuBEp-wj8IgvaZ",
+    pageId: "gNRvxN-rQ2wRw5qbBtFp1AYaxskBAyJrPwVSBgkWDbkfCcHvXPagJt7BZIad3Cj7",
     campaign_id: CAMPAIGN_ID,
     connection_id: 1,
     email: email,
@@ -2064,7 +2064,7 @@ async function createOrderViaCreditCard() {
   let orderTotal = Math.max(0, Number(selectedProduct.price) * selectedProduct.quantity);
 
   const orderData = {
-    pageId: "iDJ0JoXQ9zwd5ElWcC62pUEgCXM4CfTlmQ6MqBFtmEyGSArow_SuBEp-wj8IgvaZ",
+    pageId: "gNRvxN-rQ2wRw5qbBtFp1AYaxskBAyJrPwVSBgkWDbkfCcHvXPagJt7BZIad3Cj7",
     action: "process",
     campaign_id: CAMPAIGN_ID,
     connection_id: 1, // VRIO URL ending /connection
@@ -4103,7 +4103,7 @@ async function returnPaypal() {
 ;
 
     const body = {
-        pageId: "iDJ0JoXQ9zwd5ElWcC62pUEgCXM4CfTlmQ6MqBFtmEyGSArow_SuBEp-wj8IgvaZ",
+        pageId: "gNRvxN-rQ2wRw5qbBtFp1AYaxskBAyJrPwVSBgkWDbkfCcHvXPagJt7BZIad3Cj7",
         action: "process",
         campaign_id: CAMPAIGN_ID,
         connection_id: 1,
--- scripts/thankyou.js
+++ scripts/thankyou.js
@@ -286,7 +286,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   const endpoint =
     `orders?order_id=${orderids.join(",")}` +
-    `&with=order_offers,customer_address_billing,customer_address_shipping,customer,transactions,cart&pageId=qiLfylzhvLotSB90ySmnKTbO0q8Nxohuu03pIziizYOZOzfMocCv9H88OyViUeWj`
+    `&with=order_offers,customer_address_billing,customer_address_shipping,customer,transactions,cart&pageId=rBi-BqiXI_IsNkXYDU8TbL9bAuA-qJpzscEc0l8yQC0t534TwQsUMlyz30nO8z9q`
 
   const response = await fetch(
     `https://app-cms-api-proxy-staging-001.azurewebsites.net/vrio/${endpoint}`,
