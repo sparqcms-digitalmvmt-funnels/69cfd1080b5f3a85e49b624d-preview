1fda518ed6a9a3119b5fb836c10dab990f2cc22e
Don E <DRElliston@users.noreply.github.com>
2026-04-07 07:40:44 -0500

    deploy funnel preview

--- scripts/thankyou.js
+++ scripts/thankyou.js
@@ -286,7 +286,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   const endpoint =
     `orders?order_id=${orderids.join(",")}` +
-    `&with=order_offers,customer_address_billing,customer_address_shipping,customer,transactions,cart&pageId=TKJRC3wxaOs4lT5Fq0SBKIt__IT8RHhY6gDDtcJBwkktKVlZEaZaXzcPqVHkl66e`
+    `&with=order_offers,customer_address_billing,customer_address_shipping,customer,transactions,cart&pageId=cpRvrybqMv1ApHmUgbjd8zjbEBLHBrqvjdLyG2IfHxXotPlQKv6QgZWctbMC-0jp`
 
   const response = await fetch(
     `https://app-cms-api-proxy-staging-001.azurewebsites.net/vrio/${endpoint}`,
